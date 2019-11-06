f81df428d24b356dd0eaab9957aef778177e3a07
Hua Yang <dlyhua@cn.ibm.com>
2019-11-06 16:49:38 +0800

    modify main.js helloworld

--- src/main.js
+++ src/main.js
@@ -6,3 +6,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+// include the carbon styles and components
+import 'carbon-components/css/carbon-components.css';
+import CarbonComponentsVue from '@carbon/vue/src/index';
+Vue.use(CarbonComponentsVue);
